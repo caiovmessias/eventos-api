8c7c3f2f9211739f4df1eed1cfe2c7469a533942
Caio Messias <caio.messias@gerencianet.com.br>
2022-02-14 11:30:23 +0000

    Corrige typeorm

--- src/database/index.ts
+++ src/database/index.ts
@@ -15,9 +15,6 @@ const getOptions = async () => {
   if (process.env.DATABASE_URL) {
     Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
   } else {
-    // gets your default configuration
-    // you could get a specific config by name getConnectionOptions('production')
-    // or getConnectionOptions(process.env.NODE_ENV)
     connectionOptions = await getConnectionOptions(); 
   }
 
@@ -26,8 +23,6 @@ const getOptions = async () => {
 
 const connect2Database = async (): Promise<void> => {
     const typeormconfig = await getOptions();
-
-    console.log(typeormconfig);
     await createConnection(typeormconfig);
 };
 
